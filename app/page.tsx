f5ec9cfe03f3f41f1d290dd6a491be1f1233c526
Mariel Setton <marielsetton@Mariels-MBP.lan>
2024-05-08 11:30:58 -0400

    added 'temp city'

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,27 @@
 import Image from "next/image";
+import City from "@/components/Cities";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="fixed left-0 top-0 flex w-full justify-center mb-3 text-2xl font-semibold backdrop-blur-2xl lg:static">
+    <main className="flex flex-col">
+      <div className="flex flex-col justify-between p-14">
+        <h1 className="fixed left-0 top-0 flex mb-3 text-2xl font-semibold backdrop-blur-2xl lg:static">
           Welcome to your weather app!
         </h1>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-        <button className="bg-white rounded-full text-black text-3xl focus:outline-none focus:ring-2 focus:ring-blue-200 w-16">
-          +
-        </button>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex items-center mb-4 w-full justify-center">
+          <h2 className="text-large mr-10">
+            Your current cities
+          </h2>
+          <div className="fixed bottom-0 left-0 flex h-48 items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <button className="bg-white rounded-full text-black text-3xl focus:outline-none focus:ring-2 focus:ring-blue-200 w-16">
+            +
+          </button>
+          </div>
+        </div>
+        <div className="flex items-center mb-4 w-full justify-center">
+          <City data={true}/>
         </div>
       </div>
     </main>
---
+++ components/Cities.tsx
@@ -0,0 +1,13 @@
+
+export default function City({data}: {data:boolean}) {
+    return (
+        <div className="border m-4 p-4 rounded shadow-lg w-96">
+            <div className="text-xl">
+                City Name:
+            </div>
+            <div className="text-xl">
+            Current weather:
+            </div>
+        </div>
+    )
+}
